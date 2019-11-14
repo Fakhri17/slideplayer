8871e5128ad097cda28f3ca8c4323616b20f70bf
Mochamad Lucky Pradana <luckypradana123@yandex.ua>
2019-11-14 07:54:03 +0700

    add profile section

--- flutter_app/lib/content/ampersanda_profile.dart
+++ flutter_app/lib/content/ampersanda_profile.dart
@@ -1,6 +1,51 @@
 import 'package:flutter/material.dart';
 
-class AmpersandaProfile extends StatelessWidget {
+class AmpersandaProfile extends StatefulWidget {
+  @override
+  _AmpersandaProfileState createState() => _AmpersandaProfileState();
+}
+
+class _AmpersandaProfileState extends State<AmpersandaProfile>
+    with TickerProviderStateMixin {
+  AnimationController _controller;
+  Animation<double> profileAnimation;
+  Animation<double> nameAnimation;
+  Animation<double> jobAnimation;
+  Animation<double> socialAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 1000),
+      vsync: this,
+    )..addListener(() {
+        setState(() {});
+      });
+
+    profileAnimation = CurveTween(curve: Curves.easeOut).animate(_controller);
+
+    nameAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(CurvedAnimation(
+        parent: _controller, curve: Interval(0.2, 1.0, curve: Curves.easeIn)));
+
+    jobAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(CurvedAnimation(
+        parent: _controller, curve: Interval(0.5, 1.0, curve: Curves.easeIn)));
+
+    socialAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
+        CurvedAnimation(
+            parent: _controller,
+            curve: Interval(0.5, 1.0, curve: Curves.easeIn)));
+
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller?.dispose();
+    super.dispose();
+  }
+
   @override
   build(context) {
     return Column(
@@ -8,72 +53,96 @@ class AmpersandaProfile extends StatelessWidget {
       children: <Widget>[
         Column(
           children: <Widget>[
-            Container(
-                padding: EdgeInsets.symmetric(vertical: 32.0),
-                child: Image.asset('assets/images/profile_photo.jpeg',
-                    width: 200.0)),
-            Text(
-              'Mochamad Lucky Pradana',
-              style: TextStyle(fontSize: 32.0),
+            Opacity(
+              opacity: profileAnimation.value,
+              child: Transform.translate(
+                offset: Offset(0.0, 24.0 - (profileAnimation.value * 24.0)),
+                child: Container(
+                    padding: EdgeInsets.symmetric(vertical: 32.0),
+                    child: Image.asset('assets/images/profile_photo.jpeg',
+                        width: 200.0)),
+              ),
+            ),
+            Opacity(
+              opacity: nameAnimation.value,
+              child: Transform.translate(
+                offset: Offset(0.0, 24.0 - (nameAnimation.value * 24.0)),
+                child: Text(
+                  'Mochamad Lucky Pradana',
+                  style: TextStyle(fontSize: 32.0),
+                ),
+              ),
             ),
             SizedBox(
               height: 8.0,
             ),
-            Text(
-              'Developer at Aleph Labs',
-              style: TextStyle(fontSize: 20.0, color: Colors.grey),
+            Opacity(
+              opacity: jobAnimation.value,
+              child: Transform.translate(
+                offset: Offset(0.0, 24.0 - (nameAnimation.value * 24.0)),
+                child: Text(
+                  'Developer at Aleph Labs',
+                  style: TextStyle(fontSize: 20.0, color: Colors.grey),
+                ),
+              ),
             )
           ],
         ),
-        Container(
-          margin: EdgeInsets.only(top: 56.0, bottom: 32.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              Column(
+        Opacity(
+          opacity: socialAnimation.value,
+          child: Transform.translate(
+            offset: Offset(0.0, 24.0 - (nameAnimation.value * 24.0)),
+            child: Container(
+              margin: EdgeInsets.only(top: 56.0, bottom: 32.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: <Widget>[
-                  Image.asset(
-                    'assets/images/twitter.png',
-                    width: 48.0,
-                    filterQuality: FilterQuality.high,
+                  Column(
+                    children: <Widget>[
+                      Image.asset(
+                        'assets/images/twitter.png',
+                        width: 48.0,
+                        filterQuality: FilterQuality.high,
+                      ),
+                      SizedBox(height: 16.0),
+                      Text(
+                        '@ampersanda',
+                        style: TextStyle(fontSize: 20.0),
+                      )
+                    ],
                   ),
-                  SizedBox(height: 16.0),
-                  Text(
-                    '@ampersanda',
-                    style: TextStyle(fontSize: 20.0),
-                  )
+                  Column(
+                    children: <Widget>[
+                      Text(
+                        'w w w',
+                        style: TextStyle(fontSize: 32.0),
+                      ),
+                      SizedBox(height: 16.0),
+                      Text(
+                        'https://ampersanda.dev',
+                        style: TextStyle(fontSize: 20.0),
+                      )
+                    ],
+                  ),
+                  Column(
+                    children: <Widget>[
+                      Image.asset(
+                        'assets/images/github.png',
+                        width: 48.0,
+                        filterQuality: FilterQuality.high,
+                      ),
+                      SizedBox(height: 16.0),
+                      Text(
+                        '@ampersanda',
+                        style: TextStyle(fontSize: 20.0),
+                      )
+                    ],
+                  ),
                 ],
               ),
-              Column(
-                children: <Widget>[
-                  Text(
-                    'w w w',
-                    style: TextStyle(fontSize: 32.0),
-                  ),
-                  SizedBox(height: 16.0),
-                  Text(
-                    'https://ampersanda.dev',
-                    style: TextStyle(fontSize: 20.0),
-                  )
-                ],
-              ),
-              Column(
-                children: <Widget>[
-                  Image.asset(
-                    'assets/images/github.png',
-                    width: 48.0,
-                    filterQuality: FilterQuality.high,
-                  ),
-                  SizedBox(height: 16.0),
-                  Text(
-                    '@ampersanda',
-                    style: TextStyle(fontSize: 20.0),
-                  )
-                ],
-              ),
-            ],
+            ),
           ),
-        )
+        ),
       ],
     );
   }
